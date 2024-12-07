0a0e495a1cab9e20a02eab7696414ea8a7aaf7db
Alex Abatov <a.aliksant315@gmail.com>
2024-12-07 16:28:33 +0900

    Minor changes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
 const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
 const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "26dbb75b0378c33f9ad2a755116887a5";
 
-export { BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL, BASE_URL };
+export { BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL, BASE_URL, API_KEY_PARAM };
